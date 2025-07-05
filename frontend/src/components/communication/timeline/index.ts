696f38e941210c5fa43e8d7dfbe3e4d3da25471f
Y-RyuZU <ryuzu1006@gmail.com>
2025-07-05 11:43:53 +0000

    cleanup: remove old timeline components after reorganization

--- frontend/src/components/communication/timeline/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as TimelineFilters } from './TimelineFilters.vue'
-export { default as TimelineItem } from './TimelineItem.vue'
-export { default as TimelineItemGroup } from './TimelineItemGroup.vue'
-export { default as TimelineSkeleton } from './TimelineSkeleton.vue'
-export { default as TimelineEmptyState } from './TimelineEmptyState.vue'
-export { default as TimelineLoadMore } from './TimelineLoadMore.vue'
